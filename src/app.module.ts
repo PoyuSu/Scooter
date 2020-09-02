1cd625cad447847e5ea5dc277a2912b8ad418e18
PoyuSu <poyu1221@gmail.com>
2020-09-02 08:29:52 +0800

    Add db config to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Scooter } from './scooter/scooter.entity';
 import { ScooterModule } from './scooter/scooter.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data/scooter2.db',
+      entities: [Scooter],
+      synchronize: true,
+      logging: true,
+    }),
     ScooterModule,
   ],
   controllers: [AppController],
